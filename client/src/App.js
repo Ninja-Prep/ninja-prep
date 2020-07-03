17106cae1d338a8514bfb4caafe7f1f34271d0ad
Joanne <joanne.ye@uconn.edu>
2020-07-03 17:23:50 -0400

    hi

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ function App() {
   console.log("World");
   return <div>Hello</div>;
 }
-<<<<<<< Updated upstream
+
+function c() {
+  console.log("function c");
+}
 
 function b() {
   console.log("Joanne's function");
 }
-=======
 function A() {}
->>>>>>> Stashed changes
 export default App;
