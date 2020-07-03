607f37c3a791d39db389afdc7cf02c847434ac7a
Alex Nguyen <alexander.ngn97@gmail.com>
2020-07-03 13:40:26 -0700

    Adding both tests

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function App() {
   console.log("Lemon");
+<<<<<<< Updated upstream
   console.log("Joanne's test")
+=======
+  console.log("Alex's test");
+>>>>>>> Stashed changes
   return <div>Hello</div>;
 }
 
