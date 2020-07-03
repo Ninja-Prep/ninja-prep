141d4e8bac380d4199907396a78df3c12731cb44
Joanne <joanne.ye@uconn.edu>
2020-07-03 18:28:21 -0400

    joanne changes

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
-    return ( <
-        div > < /div>
-    )
-};
+  return <div></div>;
+}
 
 export default Header;
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,48 +1,44 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return ( <
-        nav class = "navbar navbar-expand-lg navbar-light bg-light" >
-        <
-        a class = "navbar-brand"
-        href = "#" > Navbar < /a> <
-        button class = "navbar-toggler"
-        type = "button"
-        data - toggle = "collapse"
-        data - target = "#navbarNav"
-        aria - controls = "navbarNav"
-        aria - expanded = "false"
-        aria - label = "Toggle navigation" >
-        <
-        span class = "navbar-toggler-icon" > < /span> <
-        /button> <
-        div class = "collapse navbar-collapse"
-        id = "navbarNav" >
-        <
-        ul class = "navbar-nav" >
-        <
-        li class = "nav-item active" >
-        <
-        a class = "nav-link"
-        href = "#" > Explore < span class = "sr-only" > (current) < /span></a >
-        <
-        /li> <
-        li class = "nav-item" >
-        <
-        a class = "nav-link"
-        href = "#" > Articles < /a> <
-        /li> <
-        li class = "nav-item" >
-        <
-        a class = "nav-link"
-        href = "#" > Problem < /a> <
-        /li>
-
-        <
-        /ul> <
-        /div> <
-        /nav>
-    )
-};
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <img src="images/logo.png"></img>
+      <a class="navbar-brand" href="#">
+        NinjaPrep
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Explore <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Articles
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Problems
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
