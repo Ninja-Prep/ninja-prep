278551c3110eaebeed89314fd1c6988420dba56c
Alex Nguyen <alexander.ngn97@gmail.com>
2020-07-03 11:59:29 -0700

    Adding React

---
+++ client/build/precache-manifest.898088dd19f64e220a7627b946b9eb7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80947a5e0f499c4068afaa809bc0454a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80c5d49300b71d950ac",
+    "url": "/static/js/main.624f58c6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+console.log(
+  "First path name is " + path.join(__dirname, "..", "client", "build")
+);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
-
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+console.log(
+  "The path name is " +
+    path.join(__dirname, "..", "client", "build", "index.html")
+);
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server started on port 5000");
 });
