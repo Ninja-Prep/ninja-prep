ef4f89721b3e4a9eeccbe331495dcc84c1b75348
Joanne <joanne.ye@uconn.edu>
2020-07-03 17:04:51 -0400

    joanne change

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 function App() {
-  console.log("Nguyen");
-  console.log("Hello this is Joanne here :D");
-
   console.log("Nguyen Alex");
   return <div>Hello</div>;
 }
