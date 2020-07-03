5ac9dc92d8ecf1a15a41f87f9a7a68884449a37d
Alex Nguyen <alexander.ngn97@gmail.com>
2020-07-03 14:22:19 -0700

    Adding Function A

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,12 @@ function App() {
   console.log("World");
   return <div>Hello</div>;
 }
+<<<<<<< Updated upstream
 
 function b() {
   console.log("Joanne's function");
 }
+=======
+function A() {}
+>>>>>>> Stashed changes
 export default App;
