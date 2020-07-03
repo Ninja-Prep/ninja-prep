2ddd612c201bb14ee9c22deebd38548f740041e7
Alex Nguyen <alexander.ngn97@gmail.com>
2020-07-03 13:48:30 -0700

    Adding both changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function App() {
   console.log("Adding a log statemet");
+<<<<<<< Updated upstream
   console.log("joanne's test");
+=======
+  console.log("Alex's test");
+>>>>>>> Stashed changes
   return <div>Hello</div>;
 }
 
