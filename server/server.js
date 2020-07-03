f38a0f4c47b7043d1ac192acddd2ebffc42b975b
Alex Nguyen <alexander.ngn97@gmail.com>
2020-07-03 12:10:50 -0700

    Adding React

--- server/server.js
+++ server/server.js
@@ -3,16 +3,9 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
-console.log(
-  "First path name is " + path.join(__dirname, "..", "client", "build")
-);
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
-console.log(
-  "The path name is " +
-    path.join(__dirname, "..", "client", "build", "index.html")
-);
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server started on port 5000");
 });
