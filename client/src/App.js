7db740fdc657989103a59c9ebb155d14a02d1d37
Alex Nguyen <alexander.ngn97@gmail.com>
2020-07-04 23:43:59 -0700

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "../src/Navbar";
+import Header from "../src/Header";
+
+function App() {
+  return <div> Hello Friends!</div>;
+}
+export default App;
