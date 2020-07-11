7062c53396b73db0b1965ae7b1140ef0d3f5803a
Alexander Nguyen <alxngu@amazon.com>
2020-07-11 02:24:52 -0700

    Adding ESLint for Server and React

--- client/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Navbar from "../src/Navbar";
-import Header from "../src/Header";
-
-function App() {
-  return <div> Hello Friends!</div>;
-}
-export default App;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Server started on port 5000");
+  console.log('Server started on port 5000');
 });
