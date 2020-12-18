5bf36445b1d4a6cf49470927f7c72ac6a4cd7dee
Angela Sun <asun2@uw.edu>
2020-12-17 21:01:26 -0800

    make cards

--- client/src/components/QuestionList.jsx
+++ client/src/components/QuestionList.jsx
@@ -11,29 +11,25 @@ const QuestionList = (props) => {
 	const numRows = 4
 	const numCols = 3
 
-	var card = (
-		<div class="w-25 text-white bg-secondary mb-3 col-sm p-3 mx-2">
-			<div class="card-body">
-				<h5 class="card-title">Primary card title</h5>
-			</div>
+	var questionCard = (
+		<div class="card text-primary mb-3 col-sm p-3 mx-2">
+			<blockquote class="blockquote mb-0 card-body flex-fill">
+				<p>Question</p>
+				<footer class="blockquote-footer">
+				<small class="text-muted">
+					<cite title="Stacks">Stacks 101</cite>
+				</small>
+				</footer>
+			</blockquote>
 		</div>
 	)
-
-	function copyCards() {
-		for (var i = 0; i < numCols; i++) {
-			QuestionImageList[i] = card
-		}
-		return QuestionImageList
-	}
-
-	var row = <div class="row">{copyCards()}</div>
-
-	function makeRows() {
-		let rows = []
+	
+	function makeQuestionCards() {
+		let cards = []
 		for (var i = 0; i < numRows; i++) {
-			rows[i] = row
+			cards[i] = questionCard
 		}
-		return rows
+		return cards
 	}
 
 	var easyButton = (
@@ -54,10 +50,6 @@ const QuestionList = (props) => {
 		</Link>
 	)
 
-	function makeButtons() {
-		return [easyButton, mediumButton, hardButton]
-	}
-
 	return (
 		<div className="featured-container">
 			<div className="featured-title">
@@ -65,18 +57,22 @@ const QuestionList = (props) => {
 				<PlayArrowIcon className="featured-play" />
 			</div>
 
-			{/* <div class="row mb-3 col-sm p-3 mx-auto"> 
-                    {makeButtons()}
-                </div> */}
 			<Row className="pt-3">
 				<Col className="my-auto text-center">{easyButton}</Col>
 				<Col className="my-auto text-center">{mediumButton}</Col>
 				<Col className="my-auto text-center">{hardButton}</Col>
 			</Row>
-			{/* {makeRows()} */}
+
+			<Row className="pt-3">
+				<Col className="my-auto text-center">{makeQuestionCards()}</Col>
+				<Col className="my-auto text-center">{makeQuestionCards()}</Col>
+				<Col className="my-auto text-center">{makeQuestionCards()}</Col>
+			</Row>
+			
 		</div>
 	)
 }
+
 
 export default QuestionList
 export { default as QuestionList } from './QuestionList'
