e7594158724f7a9aef8ba719c1c37dce4f6cf037
Alex Nguyen <alexander.ngn97@gmail.com>
2020-12-21 18:57:26 -0800

    Adding firstname to user accounts

--- server/server.js
+++ server/server.js
@@ -24,6 +24,10 @@ if (inProduction) {
   });
 }
 
+app.get("/", (req, res)=> {
+  res.send("Hello World!");
+})
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,19 +2,24 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
+  first_name: String,
+  premium_expiration_date: Date,
   oauth_id: String,
 });
 
-userSchema.statics.findOrCreate = async function (id, callback) {
-  let user = await this.findOne({ oauth_id: id }, (err, doc) => {
+userSchema.statics.findOrCreate = async function (profile, callback) {
+  let user = await this.findOne({ oauth_id: profile.id }, (err, doc) => {
     if (err) callback(err, null);
     else if (doc) callback(err, doc);
   });
   if (!user) {
-    user = await new this({ oauth_id: id }).save();
+    user = await new this({
+      oauth_id: profile.id,
+      first_name: profile._json.name.split(" ")[0],
+      premium_expiration_date: new Date(Date.now()),
+    }).save();
   }
+  console.log(user);
   callback(null, user);
 };
 
