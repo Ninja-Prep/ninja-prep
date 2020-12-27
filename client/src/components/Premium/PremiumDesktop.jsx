572fb8ab48765e9d8802d5c8cc9692f165266702
Joanne <joanne.ye@uconn.edu>
2020-12-26 23:01:00 -0500

    fixing merge conflicts

--- client/src/components/Premium/PremiumDesktop.jsx
+++ client/src/components/Premium/PremiumDesktop.jsx
@@ -5,23 +5,16 @@ import './premium.css'
 const PremiumDesktop = () => (
     <div className="">
         <div className="px-5 py-5 text-center header-bg">
-            <img
-                className="w-auto h-20 pb-4"
-                src="/images/NinjaPrep-Logo.png"
-                alt="logo"
-            />
+            <img className="w-auto h-20 pb-4" src="/images/NinjaPrep-Logo.png" alt="logo" />
             <h2 className="display-5 px-5 pb-2">
-                Learn, practice, and apply in-demand skills with a NinjaPrep
-                subscription.
+                Learn, practice, and apply in-demand skills with a NinjaPrep subscription.
             </h2>
             <span className="text-secondary lead w-75 m-auto d-block py-3">
                 Access to guided and convenient features, all in one platform.
             </span>
         </div>
         <div className="py-5">
-            <h2 className="display-5 px-5 pb-5 font-weight-bold text-center">
-                Our Plans
-            </h2>
+            <h2 className="display-5 px-5 pb-5 font-weight-bold text-center">Our Plans</h2>
             <CardDeck>
                 <Card>
                     <Card.Header className="bg-primary text-light">
@@ -38,10 +31,7 @@ const PremiumDesktop = () => (
                         </Row>
                     </Card.Header>
                     <Card.Body>
-                        <Card.Text>
-                            Access to guided and convenient features, all in one
-                            platform.
-                        </Card.Text>
+                        <Card.Text>Access to guided and convenient features, all in one platform.</Card.Text>
                     </Card.Body>
                     <div className="mx-auto">
                         <Button href="#" variant="outline-secondary">
@@ -61,10 +51,7 @@ const PremiumDesktop = () => (
                         </Row>
                     </Card.Header>
                     <Card.Body>
-                        <Card.Text>
-                            Access to guided and convenient features, all in one
-                            platform.
-                        </Card.Text>
+                        <Card.Text>Access to guided and convenient features, all in one platform.</Card.Text>
                     </Card.Body>
                     <div className="mx-auto">
                         <Button href="#" variant="outline-secondary">
@@ -84,10 +71,7 @@ const PremiumDesktop = () => (
                         </Row>
                     </Card.Header>
                     <Card.Body>
-                        <Card.Text>
-                            Access to guided and convenient features, all in one
-                            platform.
-                        </Card.Text>
+                        <Card.Text>Access to guided and convenient features, all in one platform.</Card.Text>
                     </Card.Body>
                     <div className="mx-auto">
                         <Button href="#" variant="outline-secondary">
