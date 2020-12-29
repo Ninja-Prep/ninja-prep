b6ab5ef13d0504a139d4d1541eb3b443a5af50ec
Joanne <joanne.ye@uconn.edu>
2020-12-29 15:54:00 -0500

    adding up to date master branch

--- client/src/components/Premium/PremiumDesktop/Benefits.jsx
+++ client/src/components/Premium/PremiumDesktop/Benefits.jsx
@@ -20,15 +20,16 @@ function Benefits() {
                         <DoneIcon className="align-middle" style={styles.doneIcon} />
                         <p className="d-inline lead align-middle">Access to Premium Content</p>
                         <p className="text-secondary lead pt-2">
-                            Access to over 100 challenges that cover in-demand algorithm and data structure concepts. This is placeholder text where you elaborate on the benefit in about 2 to 3 sentences.
-
+                            Access to over 100 challenges that cover in-demand algorithm and data structure concepts.
+                            This is placeholder text where you elaborate on the benefit in about 2 to 3 sentences.
                         </p>
                     </Col>
                     <Col className="pl-5">
                         <DoneIcon style={styles.doneIcon} />
                         <p className="d-inline lead align-middle">Premium Articles</p>
                         <p className="text-secondary lead pt-2">
-                            This is placeholder text where you elaborate on the benefit. Elaborate articles to provide detailed conceptual overview.  This is placeholder text where you elaborate on the benefit. 
+                            This is placeholder text where you elaborate on the benefit. Elaborate articles to provide
+                            detailed conceptual overview. This is placeholder text where you elaborate on the benefit.
                         </p>
                     </Col>
                 </Row>
@@ -37,15 +38,18 @@ function Benefits() {
                         <DoneIcon style={styles.doneIcon} />
                         <p className="d-inline lead align-middle">Code Walkthroughs</p>
                         <p className="text-secondary lead pt-2">
-                            Step by Step video solutions to efficiently walk you through the problem. This is placeholder text where you elaborate on the benefit. Elaborate articles to provide detailed conceptual overview. This is placeholder text where you elaborate on the benefit in about 2 to 3 sentences. 
-
+                            Step by Step video solutions to efficiently walk you through the problem. This is
+                            placeholder text where you elaborate on the benefit. Elaborate articles to provide detailed
+                            conceptual overview. This is placeholder text where you elaborate on the benefit in about 2
+                            to 3 sentences.
                         </p>
                     </Col>
                     <Col className="pl-5">
                         <DoneIcon style={styles.doneIcon} />
                         <p className="d-inline lead align-middle">Crash Course</p>
                         <p className="text-secondary lead pt-2">
-                        This is placeholder text where you elaborate on the benefit. Elaborate articles to provide detailed conceptual overview.  This is placeholder text where you elaborate on the benefit. 
+                            This is placeholder text where you elaborate on the benefit. Elaborate articles to provide
+                            detailed conceptual overview. This is placeholder text where you elaborate on the benefit.
                         </p>
                     </Col>
                 </Row>
@@ -54,14 +58,16 @@ function Benefits() {
                         <DoneIcon style={styles.doneIcon} />
                         <p className="d-inline lead align-middle">In-Browser Compiler</p>
                         <p className="text-secondary lead pt-2">
-                        This is placeholder text where you elaborate on the benefit. Elaborate articles to provide detailed conceptual overview.  This is placeholder text where you elaborate on the benefit. 
+                            This is placeholder text where you elaborate on the benefit. Elaborate articles to provide
+                            detailed conceptual overview. This is placeholder text where you elaborate on the benefit.
                         </p>
                     </Col>
                     <Col className="pl-5">
                         <DoneIcon style={styles.doneIcon} />
                         <p className="d-inline lead align-middle">Code in 5 languages</p>
                         <p className="text-secondary lead pt-2">
-                        This is placeholder text where you elaborate on the benefit. Elaborate articles to provide detailed conceptual overview.  This is placeholder text where you elaborate on the benefit. 
+                            This is placeholder text where you elaborate on the benefit. Elaborate articles to provide
+                            detailed conceptual overview. This is placeholder text where you elaborate on the benefit.
                         </p>
                     </Col>
                 </Row>
