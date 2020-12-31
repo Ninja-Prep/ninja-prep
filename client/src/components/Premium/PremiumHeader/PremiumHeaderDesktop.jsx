4c642d6f4965e0daaca8ceb1840ab2fe24464b62
Joanne <joanne.ye@uconn.edu>
2020-12-30 19:32:13 -0500

    change stringIds naming convention

--- client/src/components/Premium/PremiumHeader/PremiumHeaderDesktop.jsx
+++ client/src/components/Premium/PremiumHeader/PremiumHeaderDesktop.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../premium.css'
-import { HEADER_TITLE, HEADER_DESCRIPTION } from './PremiumHeaderStringIds'
+import { PREMIUM_HEADER_TITLE, PREMIUM_HEADER_DESCRIPTION } from './PremiumHeaderStringIds'
 
 function PremiumHeaderDesktop() {
     return (
         <div className="py-5 text-center header-bg">
             <img className="w-auto pb-4 pt-2" src="/images/NinjaPrep-Logo.png" alt="logo" />
-            <h2 className="display-5 pb-2">{HEADER_TITLE}</h2>
-            <span className="text-secondary lead m-auto d-block ">{HEADER_DESCRIPTION}</span>
+            <h2 className="display-5 pb-2">{PREMIUM_HEADER_TITLE}</h2>
+            <span className="text-secondary lead m-auto d-block ">{PREMIUM_HEADER_DESCRIPTION}</span>
         </div>
     )
 }
--- client/src/components/Premium/PremiumHeader/PremiumHeaderStringIds.jsx
+++ client/src/components/Premium/PremiumHeader/PremiumHeaderStringIds.jsx
@@ -1,2 +1,2 @@
-export const HEADER_TITLE = 'Learn, practice, and apply in-demand skills with a NinjaPrep subscription.'
-export const HEADER_DESCRIPTION = 'Access to guided and convenient features, all in one platform.'
+export const PREMIUM_HEADER_TITLE = 'Learn, practice, and apply in-demand skills with a NinjaPrep subscription.'
+export const PREMIUM_HEADER_DESCRIPTION = 'Access to guided and convenient features, all in one platform.'
