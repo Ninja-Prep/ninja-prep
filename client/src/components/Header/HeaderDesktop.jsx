1e25469b12eb5eff4cb640a50dad5902621f606d
Joanne <joanne.ye@uconn.edu>
2021-01-04 16:47:32 -0500

    creating map function to create Journey List and condensed css for Journey component

--- client/src/components/Header/HeaderDesktop.jsx
+++ client/src/components/Header/HeaderDesktop.jsx
@@ -33,7 +33,7 @@ const HeaderDesktop = () => (
     <div className="header-bg text-light">
         <Container className=" header-container">
             <Row>
-                <Col className="pr-5 mr-5 my-auto">
+                <Col className="pr-5 pl-0 mx-5 my-auto">
                     <div>
                         <h3 className="font-weight-bold pb-2">{ENHANCE_YOUR_SKILLS}</h3>
                         <p className="lead">
@@ -41,7 +41,7 @@ const HeaderDesktop = () => (
                         </p>
                     </div>
                 </Col>
-                <Col className="pl-5 ml-5">
+                <Col className="pl-5 pr-0 mx-5">
                     <div className="form-bg rounded">
                         <div className="pt-4 pb-2 px-5">
                             <h4 className="font-weight-bold pb-3 mx-auto text-light">{GET_STARTED}</h4>
