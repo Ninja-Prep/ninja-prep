e998cb2ddda183baaea6bbf7256fba941d8277a7
Angela Sun <asun2@uw.edu>
2021-01-16 11:56:34 -0800

    start icons

--- client/src/components/QuestionList/QuestionListDesktop.jsx
+++ client/src/components/QuestionList/QuestionListDesktop.jsx
@@ -5,72 +5,84 @@ import { Link } from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap'
 
 import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 
 const QuestionListDesktop = (props) => {
-	const numRows = 4
+    const numRows = 4
 
-	const questionCard = (
-		<div className="card text-primary mb-3 col-sm p-3 mx-2">
-			<blockquote className="blockquote mb-0 card-body flex-fill">
-				<p>Question</p>
-				<footer className="blockquote-footer">
+    let icon = <CheckCircleIcon className="check-circle-icon" />
+
+    const questionCard = (
+        <div className="card flex-row flex-wrap mb-3 col-sm p-0">
+            <div className="img-square-wrapper p-2">{icon}</div>
+            <div className="card-block px-2">
+                <blockquote className="mb-0 card-body flex-fill">
+                    <p>Question</p>
+                    {/* <footer className="blockquote-footer">
 				<small className="text-muted">
 					<cite title="Stacks">Stacks 101</cite>
 				</small>
-				</footer>
-			</blockquote>
-		</div>
-	)
-	
-	function makeQuestionCards() {
-		let cards = []
-		for (var i = 0; i < numRows; i++) {
-			cards[i] = questionCard
-		}
-		return cards
-	}
+				</footer> */}
+                </blockquote>
+            </div>
+        </div>
+    )
 
-	const easyButton = (
-		<Link className="nav-link" to="#">
-			<p className="easy d-inline">Easy</p>
-		</Link>
-	)
+    function makeQuestionCard(title, status) {
+        if (status == null) {
+            icon = null
+        }
+    }
 
-	const mediumButton = (
-		<Link className="nav-link" to="#">
-			<p className="medium d-inline">Medium</p>
-		</Link>
-	)
+    function makeQuestionCards() {
+        let cards = []
+        // TODO: get question list from db here
+        let questionList = ['']
+        for (let i = 0; i < numRows; i++) {
+            cards[i] = questionCard
+        }
+        return cards
+    }
 
-	const hardButton = (
-		<Link className="nav-link" to="#">
-			<p className="hard d-inline">Hard</p>
-		</Link>
-	)
+    const easyButton = (
+        <Link className="nav-link" to="#">
+            <p className="easy d-inline">Easy</p>
+        </Link>
+    )
 
-	return (
-		<div className="featured-container">
-			<div className="featured-title">
-				<h1 className="bold">All</h1>
-				<PlayArrowIcon className="featured-play" />
-			</div>
+    const mediumButton = (
+        <Link className="nav-link" to="#">
+            <p className="medium d-inline">Medium</p>
+        </Link>
+    )
 
-			<Row className="pt-3">
-				<Col className="my-auto text-center">{easyButton}</Col>
-				<Col className="my-auto text-center">{mediumButton}</Col>
-				<Col className="my-auto text-center">{hardButton}</Col>
-			</Row>
+    const hardButton = (
+        <Link className="nav-link" to="#">
+            <p className="hard d-inline">Hard</p>
+        </Link>
+    )
 
-			<Row className="pt-3">
-				<Col className="my-auto text-center">{makeQuestionCards()}</Col>
-				<Col className="my-auto text-center">{makeQuestionCards()}</Col>
-				<Col className="my-auto text-center">{makeQuestionCards()}</Col>
-			</Row>
-			
-		</div>
-	)
+    return (
+        <div className="featured-container">
+            <div className="featured-title">
+                <h1 className="bold">All</h1>
+                <PlayArrowIcon className="featured-play" />
+            </div>
+
+            <Row className="pt-3">
+                <Col className="my-auto text-center">{easyButton}</Col>
+                <Col className="my-auto text-center">{mediumButton}</Col>
+                <Col className="my-auto text-center">{hardButton}</Col>
+            </Row>
+
+            <Row className="pt-3">
+                <Col className="my-auto text-center">{makeQuestionCards()}</Col>
+                <Col className="my-auto text-center">{makeQuestionCards()}</Col>
+                <Col className="my-auto text-center">{makeQuestionCards()}</Col>
+            </Row>
+        </div>
+    )
 }
-
 
 export default QuestionListDesktop
 export { default as QuestionListDesktop } from './QuestionListDesktop'
