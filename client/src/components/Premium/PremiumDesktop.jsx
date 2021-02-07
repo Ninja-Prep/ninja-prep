ecddc9b0c79f309ac0b222e54e3a3acdef86ebce
Angela Sun <asun2@uw.edu>
2021-02-06 21:45:03 -0800

    stripe integration with premium page

--- client/src/components/Premium/PremiumDesktop.jsx
+++ client/src/components/Premium/PremiumDesktop.jsx
@@ -5,11 +5,20 @@ import { BenefitsDesktop } from './Benefits/BenefitsDesktop'
 import { PremiumFAQDesktop } from './PremiumFAQ/PremiumFAQDesktop'
 import './premium.css'
 
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
 function PremiumDesktop() {
+    const stripePromise = loadStripe(
+        'pk_test_51IFPpVD8LTDC9rJz1d5GqoT9IEMvFup6qRVsrORslhqUGRrDWbeGludlqWIDhW9RwrRRUCNlHZ5JCksGo7IOqD5q00JGnjPjA3'
+    )
+
     return (
         <div className="circles-bg">
             <PremiumHeaderDesktop />
-            <PlansDesktop />
+            <Elements stripe={stripePromise}>
+                <PlansDesktop />
+            </Elements>
             <BenefitsDesktop />
             <PremiumFAQDesktop />
         </div>
