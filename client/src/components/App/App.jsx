3725216a4ea64f33fd83ee10eddf4f797efb143b
JoanneCheong <joanne.ye@uconn.edu>
2021-03-19 21:12:24 -0400

    fixing merge conflicts

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import NavigationBarComponent from '../Partials/NavigationBar/NavigationBarComponent'
 import LandingPage from '../../views/LandingPage'
 import PremiumPage from '../../views/PremiumPage'
 import ChallengesPage from '../../views/ChallengesPage'
@@ -10,7 +9,7 @@ import LoginPage from '../../views/LoginPage'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ProtectedRoute from '../Routes/ProtectedRoute'
 import RouteWrapper from '../Routes/RouteWrapper'
-import { FooterLayout } from '../Routes/Layouts'
+import { HeaderandFooterLayout } from '../Routes/Layouts'
 import ProblemTemplatePage from '../../views/ProblemTemplatePage'
 
 class App extends Component {
@@ -24,14 +23,14 @@ class App extends Component {
                         exact
                         authComponent={ChallengesPage}
                         component={LoginPage}
-                        layout={FooterLayout}
+                        layout={HeaderandFooterLayout}
                     />
                     <ProtectedRoute
                         path="/"
                         exact
                         authComponent={ChallengesPage}
                         component={LandingPage}
-                        layout={FooterLayout}
+                        layout={HeaderandFooterLayout}
                     />
                     <RouteWrapper path="/challenges" exact component={ChallengesPage} layout={FooterLayout} />
                     <RouteWrapper path="/about" exact component={MeetTheTeamPage} layout={FooterLayout} />
